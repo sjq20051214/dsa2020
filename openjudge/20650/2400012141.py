8690c826d13fd675e6fa59de47f82b77cfc04932
sjq20051214 <1542084033@qq.com>
2025-03-18 17:04:37 +0800

    Add files via upload

---
+++ openjudge/20650/2400012141.py
@@ -0,0 +1,13 @@
+def zuichanggonggong(a: str, b: str) -> int:
+    m, n = len(a), len(b)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if a[i - 1] == b[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    return dp[m][n]  # 返回最终 LCS 长度
+list1 = input().strip()
+list2 = input().strip()
+print(zuichanggonggong(list1, list2))
